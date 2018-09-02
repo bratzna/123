ae16f468025db8740190e76d605764dc819dede4
bratzna <42917284+bratzna@users.noreply.github.com>
2018-09-03 00:42:05 +0200

    Create tim-dodavanje.php

---
+++ tim-dodavanje.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dodavanje tima</title>
+</head>
+<body>
+    <h1>Dodavanje tima</h1>
+    <form action="tim-dodavanje-db.php" method="post">
+        Naziv: <input type="text" name="naziv" />
+        <hr />
+        <button type="submit">Dodavanje</button>
+    </form>
+</body>
+</html>
