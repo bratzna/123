feb370c286f838b0e6c7f220ae62477e3ef9e046
bratzna <42917284+bratzna@users.noreply.github.com>
2018-09-03 00:41:21 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <a href="tim-pregled.php">Pregled tima</a>
+    <a href="igrac-pregled.php">Pregled igraca</a>
+</body>
+</html>
